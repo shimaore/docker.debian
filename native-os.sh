e727318fce904913ee56e55945f009792e7522bb
Stephane Alnet <stephane@shimaore.net>
2014-09-11 20:15:00 +0200

    Added `scratch` creation.

--- native-os.sh
+++ native-os.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
-# See also
-#   sudo TMPDIR="$PWD" DONT_TOUCH_SOURCES_LIST=true /usr/share/docker.io/contrib/mkimage.sh -t shimaore/debian debootstrap --variant=minbase testing
 
-# This script must be ran as root, both debootstrap and docker.io need it.
-
-MIRROR=debian.shimaore.net:9999
-DIST=testing
-# Comma-separated list of packages to add to minbase, which includes only essential packages and apt.
-EXTRA_PACKAGES=""
+if docker inspect scratch > /dev/null; then
+  echo "Using existing scratch image";
+else
+  # scratch.tar is a tar file containing
+  #   drwxr-xr-x root/root         0 2014-09-11 19:51 ./
+  docker import - scratch < scratch.tar
+fi
 
 DATE=`/bin/date -u '+%Y%m%d-%H%M'`
-TMPDIR=`mktemp -d --tmpdir=$PWD`
-
-echo "Using TMPDIR=${TMPDIR}"
-[ -d "${TMPDIR}" ] || exit 1
-
-/usr/sbin/debootstrap --arch=amd64 --variant=minbase "${DIST}" "${TMPDIR}" "http://${MIRROR}/debian" || exit 1
-exit 0
-
-# Based on mkimage.sh
-rm -rf "${TMPDIR}/dev" "${TMPDIR}/proc"
-
-/bin/tar -c -C "${TMPDIR}" . | /usr/bin/docker.io import - "debian-${DIST}:${DATE}"
-
-echo "Removing ${TMPDIR}"
-rm -rf "${TMPDIR}"
