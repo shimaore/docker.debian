ee9e7d67c348ac0eecd29578fcd0df514fc2b77d
Stephane Alnet <stephane@shimaore.net>
2014-09-11 20:33:01 +0200

    No. Tabs.

--- mkimage.sh
+++ mkimage.sh
@@ -4,12 +4,12 @@ set -e
 mkimg="$(basename "$0")"
 
 usage() {
-	echo >&2 "usage: $mkimg [-d dir] [-t tag] script [script-args]"
-	echo >&2 "   ie: $mkimg -t someuser/debian debootstrap --variant=minbase jessie"
-	echo >&2 "       $mkimg -t someuser/ubuntu debootstrap --include=ubuntu-minimal trusty"
-	echo >&2 "       $mkimg -t someuser/busybox busybox-static"
-	echo >&2 "       $mkimg -t someuser/centos:5 rinse --distribution centos-5"
-	exit 1
+  echo >&2 "usage: $mkimg [-d dir] [-t tag] script [script-args]"
+  echo >&2 "   ie: $mkimg -t someuser/debian debootstrap --variant=minbase jessie"
+  echo >&2 "       $mkimg -t someuser/ubuntu debootstrap --include=ubuntu-minimal trusty"
+  echo >&2 "       $mkimg -t someuser/busybox busybox-static"
+  echo >&2 "       $mkimg -t someuser/centos:5 rinse --distribution centos-5"
+  exit 1
 }
 
 scriptDir="$(dirname "$(readlink -f "$BASH_SOURCE")")/mkimage"
@@ -21,12 +21,12 @@ unset optTemp
 dir=
 tag=
 while true; do
-	case "$1" in
-		-d|--dir) dir="$2" ; shift 2 ;;
-		-t|--tag) tag="$2" ; shift 2 ;;
-		-h|--help) usage ;;
-		--) shift ; break ;;
-	esac
+  case "$1" in
+    -d|--dir) dir="$2" ; shift 2 ;;
+    -t|--tag) tag="$2" ; shift 2 ;;
+    -h|--help) usage ;;
+    --) shift ; break ;;
+  esac
 done
 
 script="$1"
@@ -34,22 +34,22 @@ script="$1"
 shift
 
 if [ ! -x "$scriptDir/$script" ]; then
-	echo >&2 "error: $script does not exist or is not executable"
-	echo >&2 "  see $scriptDir for possible scripts"
-	exit 1
+  echo >&2 "error: $script does not exist or is not executable"
+  echo >&2 "  see $scriptDir for possible scripts"
+  exit 1
 fi
 
 # don't mistake common scripts like .febootstrap-minimize as image-creators
 if [[ "$script" == .* ]]; then
-	echo >&2 "error: $script is a script helper, not a script"
-	echo >&2 "  see $scriptDir for possible scripts"
-	exit 1
+  echo >&2 "error: $script is a script helper, not a script"
+  echo >&2 "  see $scriptDir for possible scripts"
+  exit 1
 fi
 
 delDir=
 if [ -z "$dir" ]; then
-	dir="$(mktemp -d ${TMPDIR:-/tmp}/docker-mkimage.XXXXXXXXXX)"
-	delDir=1
+  dir="$(mktemp -d ${TMPDIR:-/tmp}/docker-mkimage.XXXXXXXXXX)"
+  delDir=1
 fi
 
 rootfsDir="$dir/rootfs"
@@ -73,8 +73,8 @@ tarFile="$dir/rootfs.tar.xz"
 touch "$tarFile"
 
 (
-	set -x
-	tar --numeric-owner -caf "$tarFile" -C "$rootfsDir" --transform='s,^./,,' .
+  set -x
+  tar --numeric-owner -caf "$tarFile" -C "$rootfsDir" --transform='s,^./,,' .
 )
 
 echo >&2 "+ cat > '$dir/Dockerfile'"
@@ -85,21 +85,21 @@ EOF
 
 # if our generated image has a decent shell, let's set a default command
 for shell in /bin/bash /usr/bin/fish /usr/bin/zsh /bin/sh; do
-	if [ -x "$rootfsDir/$shell" ]; then
-		( set -x; echo 'CMD ["'"$shell"'"]' >> "$dir/Dockerfile" )
-		break
-	fi
+  if [ -x "$rootfsDir/$shell" ]; then
+    ( set -x; echo 'CMD ["'"$shell"'"]' >> "$dir/Dockerfile" )
+    break
+  fi
 done
 
 ( set -x; rm -rf "$rootfsDir" )
 
 if [ "$tag" ]; then
-	( set -x; docker build -t "$tag" "$dir" )
+  ( set -x; docker build -t "$tag" "$dir" )
 elif [ "$delDir" ]; then
-	# if we didn't specify a tag and we're going to delete our dir, let's just build an untagged image so that we did _something_
-	( set -x; docker build "$dir" )
+  # if we didn't specify a tag and we're going to delete our dir, let's just build an untagged image so that we did _something_
+  ( set -x; docker build "$dir" )
 fi
 
 if [ "$delDir" ]; then
-	( set -x; rm -rf "$dir" )
+  ( set -x; rm -rf "$dir" )
 fi
