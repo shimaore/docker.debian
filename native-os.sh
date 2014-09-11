c630d4224aba1f45b2e5c5c0b0a7825730b37e9e
Stephane Alnet <stephane@shimaore.net>
2014-09-11 21:21:08 +0200

    Add `latest` tag automatically.

--- native-os.sh
+++ native-os.sh
@@ -2,3 +2,5 @@
 
 DATE=`/bin/date -u '+%Y%m%d-%H%M%S'`
 TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://debian.shimaore.net:9999/debian
+
+docker tag "shimaore/debian:$DATE" shimaore/debian:latest
