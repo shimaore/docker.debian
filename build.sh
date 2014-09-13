01fddda3bc723b7e81c22f361bc5153662bdd23e
Stephane Alnet <stephane@shimaore.net>
2014-09-14 00:58:51 +0200

    Add tests.

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -e
 
 DATE=`/bin/date -u '+%Y%m%d-%H%M%S'`
 TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://debian.shimaore.net:9999/debian
 
 docker tag "shimaore/debian:$DATE" shimaore/debian:latest
+
+echo Tests
+for test in test/*; do "$test"; done
