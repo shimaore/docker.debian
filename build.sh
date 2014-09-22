737d3508ced5c1614ee08d5f7822cf3ccb4a9ee9
Stephane Alnet <stephane@shimaore.net>
2014-09-22 23:44:59 +0200

    Corrected location

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 DATE=`/bin/date -u '+%Y%m%d-%H%M%S'`
-TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://debian.shimaore.net:9999/debian
+TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://debian.shimaore.net/debian
 
 docker tag "shimaore/debian:$DATE" shimaore/debian:latest
 
