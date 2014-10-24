d48af32226c585d4b5bc62529121ba9e4b711a87
Stephane Alnet <stephane@shimaore.net>
2014-10-24 13:07:21 +0200

    Use proper source

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 set -e
 
 DATE=`/bin/date -u '+%Y%m%d-%H%M%S'`
-TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://debian.shimaore.net/debian
+# TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://debian.shimaore.net/debian
+TMPDIR=./mkimage ./mkimage.sh -t "shimaore/debian:$DATE" debootstrap --variant=minbase testing http://ftp.fr.debian.org/debian
 
 docker tag "shimaore/debian:$DATE" shimaore/debian:latest
 
