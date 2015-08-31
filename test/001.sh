513640e709e8f62b406404aa31f00da26de1294d
Stephane Alnet <stephane@shimaore.net>
2015-08-31 12:29:28 +0200

    Test with the specific version.

--- test/001.sh
+++ test/001.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm=true shimaore/debian /bin/bash <<'EOF' && echo OK
+docker run --rm=true shimaore/debian:`jq -r .version package.json` /bin/bash <<'EOF' && echo OK
   [ -x /etc/resolv.conf ] || exit 1
   [ -x /etc/apt/sources.list ] || exit 1
   [ -x /etc/hostname ] || exit 1
